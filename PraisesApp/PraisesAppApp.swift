654e7160a49c598b7cbb5edd8191094be0245e68
Sanura <sanurajerome@gmail.com>
2023-08-20 10:25:30 -0500

    Initial Commit

---
+++ PraisesApp/PraisesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PraisesAppApp.swift
+//  PraisesApp
+//
+//  Created by sanura jerome on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct PraisesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
